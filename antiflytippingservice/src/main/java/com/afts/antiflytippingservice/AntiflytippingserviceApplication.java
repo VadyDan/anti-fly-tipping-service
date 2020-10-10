e19548a174938bfcfd90830d0e2b6fdec0f94379
MaksimZayakin <71225726+MaksimZayakin@users.noreply.github.com>
2020-10-10 20:33:31 +0500

    SpringInitializeProject

---
+++ antiflytippingservice/src/main/java/com/afts/antiflytippingservice/AntiflytippingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.afts.antiflytippingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiflytippingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiflytippingserviceApplication.class, args);
+	}
+
+}
---
+++ antiflytippingservice/src/test/java/com/afts/antiflytippingservice/AntiflytippingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afts.antiflytippingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiflytippingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
